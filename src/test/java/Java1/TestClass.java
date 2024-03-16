ed1c89bc330c69d41835488ba5ead9866f27747d
MadhulikaProject <madhulikamalik00@gmail.com>
2024-03-16 13:02:28 +0530

    my 1st

---
+++ src/test/java/Java1/TestClass.java
@@ -0,0 +1,14 @@
+package Java1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      WebDriver driver = new ChromeDriver ();
+      driver.get("https://google.com");
+	}
+
+}
